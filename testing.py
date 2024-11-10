caa92641579bed0b7a95e1544fab7d0d4dd106ac
Jasper Bo <jjbo@wisc.edu>
2024-11-09 20:21:19 -0600

    created initial ReceiptReader class

--- testing.py
+++ testing.py
@@ -4,9 +4,29 @@ import cv2
 import enum 
 import re
 
+class ReceiptReader():
+    def __init__(self):
+        self.image = None
+        
+    def reader(self):
+        print("ha")
+        self.image = cv2.imread("/Users/jasperbo/Desktop/madhack/Print_Payment_Receipt.JPG", cv2.IMREAD_GRAYSCALE)
+        # img = Image.open("/Users/2monkey3/Desktop/madhack/Print_Payment_Receipt.JPG")
+        extracted_text = pytesseract.image_to_string(self.image, lang='eng')
+        numbers = re.findall(r"\d+\.{1}\d+", extracted_text)
+        total = float(numbers[-1])
+        # print(extracted_text)
+        print(numbers)
+        print(extracted_text.strip().split("\n"))
+        text_array = extracted_text.strip().split("\n")
+        title = text_array[0]
+        print(title)
+        print(total)
+        return title, total
+        
 def main():
     print("ha")
-    grayscale_image = cv2.imread("/Users/2monkey3/Desktop/madhack/Print_Payment_Receipt.JPG", cv2.IMREAD_GRAYSCALE)
+    grayscale_image = cv2.imread("/Users/jasperbo/Desktop/madhack/Print_Payment_Receipt.JPG", cv2.IMREAD_GRAYSCALE)
     # img = Image.open("/Users/2monkey3/Desktop/madhack/Print_Payment_Receipt.JPG")
     extracted_text = pytesseract.image_to_string(grayscale_image, lang='eng')
     numbers = re.findall(r"\d+\.{1}\d+", extracted_text)
@@ -17,10 +37,7 @@ def main():
     text_array = extracted_text.strip().split("\n")
     title = text_array[0]
     print(title)
-    print(total)
-
-# def extract_image(image, lang):
-        
+    print(total)     
 
 # This block ensures main() is called only when the script is run directly
 if __name__ == "__main__":
