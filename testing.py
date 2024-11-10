cdfe107b4a1700f06a204040212f1a7508bb0c2c
Jasper Bo <jjbo@wisc.edu>
2024-11-10 01:27:56 -0600

    Deleted extra main function code in testing.py

--- testing.py
+++ testing.py
@@ -13,6 +13,7 @@ class ReceiptReader():
         self.image = cv2.imread(self.image, cv2.IMREAD_GRAYSCALE)
         img = Image.open("tmp_image.png")
         extracted_text = pytesseract.image_to_string(img, lang='eng')
+        print(extracted_text)
         numbers = re.findall(r"\d+\.{1}\d+", extracted_text)
         total = float(numbers[-1])
         # print(extracted_text)
@@ -24,21 +25,3 @@ class ReceiptReader():
         print(total)
         return title, total
         
-def main():
-    print("ha")
-    grayscale_image = cv2.imread("/Users/jasperbo/Desktop/madhack/Print_Payment_Receipt.JPG", cv2.IMREAD_GRAYSCALE)
-    # img = Image.open("/Users/2monkey3/Desktop/madhack/Print_Payment_Receipt.JPG")
-    extracted_text = pytesseract.image_to_string(grayscale_image, lang='eng')
-    numbers = re.findall(r"\d+\.{1}\d+", extracted_text)
-    total = float(numbers[-1])
-    # print(extracted_text)
-    print(numbers)
-    print(extracted_text.strip().split("\n"))
-    text_array = extracted_text.strip().split("\n")
-    title = text_array[0]
-    print(title)
-    print(total)     
-
-# This block ensures main() is called only when the script is run directly
-if __name__ == "__main__":
-    main()
